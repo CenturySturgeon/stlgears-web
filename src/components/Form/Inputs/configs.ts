c13a570e1d88d5e81dea41d74476c116095e43b8
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2026-05-02 15:50:22 -0600

    Centralized configs

--- src/components/Form/Inputs/configs.ts
+++ src/components/Form/Inputs/configs.ts
@@ -12,10 +12,10 @@ export const moduleInputConfig = {
         max: 75,
         decimalScale: 2,
     },
-    helpText: 'Your public name',
+    helpText: 'The module controls the size of the tooth and thus the total size of the gear.',
     helpImage: "/images/gears/inputs/module.svg",
     helpLink: {
-        href: '/',
+        href: '/theory/module',
         label: 'Learn more'
     }
 };
@@ -32,7 +32,7 @@ export const pressureAngleInputConfig = {
         max: 35,
         decimalScale: 1,
     },
-    helpText: 'Your public name',
+    helpText: 'The pressure angle is the angle between the line of action and the tangent to the pitch circle, typically 20 or 25 degrees.',
     helpImage: "/images/gears/inputs/pressure_angle.svg",
     helpLink: {
         href: '/',
@@ -64,8 +64,67 @@ export const lengthInputConfig = {
         decimalScale: 2,
     },
     helpImage: "/images/gears/inputs/spur_length.svg",
-    helpLink: {
-        href: '/',
-        label: 'Learn more'
-    }
+    helpText: "The distance between the gear's bottom and top face."
+};
+
+export const radialThicknessInputConfig = {
+    InputComponent: NumberInput,
+    inputProps: {
+        label: 'Radial thickness',
+        defaultValue: 10,
+        placeholder: 'mm',
+        suffix: ' mm',
+        min: 0.05,
+        max: 400,
+        decimalScale: 2,
+    },
+    helpImage: "/images/gears/inputs/radial_thickness.svg",
+    helpText: "Distance between the root diameter (closest to the cylindrical face) and the outer diameter.",
+};
+
+export const doubleHelicalLengthInputConfig = {
+    ...lengthInputConfig,
+    helpImage: "/images/gears/inputs/double_helical_length.svg"
+}
+export const helicalLengthInputConfig = {
+    ...lengthInputConfig,
+    helpImage: "/images/gears/inputs/helical_length.svg"
+}
+
+export const rackModuleInputConfig = {
+    ...moduleInputConfig,
+    helpImage: "/images/gears/inputs/rack_module.svg",
+    helpText: "The module controls the distance from the bottom to the tip of the tooth."
+}
+
+export const rackPressureAngleInputConfig = {
+    ...pressureAngleInputConfig,
+    helpImage: "/images/gears/inputs/rack_pressure_angle.svg",
+    helpText: "The pressure angle controls the inclinaction of the tooth\'s trapezoidal leg meassured from a line perpendicular to the base."
+}
+
+export const rackNumberOfTeethInputConfig = {
+    ...numberOfTeethInputConfig,
+    helpImage: "/images/gears/inputs/rack_length.svg",
+    helpText: "The rack's length is defined by the number of teeth."
+}
+
+export const rackLengthInputConfig = {
+    ...lengthInputConfig,
+    helpImage: "/images/gears/inputs/rack_length.svg",
+    helpText: "The rack's total number of teeth is in function of the length its base."
+}
+
+export const rackWidthInputConfig = {
+    ...lengthInputConfig,
+    inputProps: { ...lengthInputConfig.inputProps, label: 'Base width' },
+    helpImage: "/images/gears/inputs/rack_width.svg",
+    helpText: "Width of the rack\'s base."
+};
+
+export const rackBaseHeightInputConfig = {
+    ...lengthInputConfig,
+    inputProps: { ...lengthInputConfig.inputProps, label: 'Base height' },
+    helpImage: "/images/gears/inputs/rack_base_height.svg",
+    helpText: "Distance between the base and the bottom of the teeth."
 };
