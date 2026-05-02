9c246dcfa21ca6c35cf4e5770a558b35eb7a8a3c
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2026-05-02 15:50:41 -0600

    Generators use input configs

--- src/app/generators/dxf/config.ts
+++ src/app/generators/dxf/config.ts
@@ -1,17 +1,32 @@
+import {
+  moduleInputConfig,
+  pressureAngleInputConfig,
+  numberOfTeethInputConfig,
+  radialThicknessInputConfig,
+  rackModuleInputConfig,
+  rackPressureAngleInputConfig,
+  rackLengthInputConfig,
+  rackNumberOfTeethInputConfig,
+  rackBaseHeightInputConfig,
+} from "@/components/Form/Inputs/configs";
+
 export const cardsData = [
   {
     image: "/images/gears/internal_spur.png",
     title: "Internal Spur Gear",
     description: "Something else",
+    inputConfigs: [moduleInputConfig, pressureAngleInputConfig, numberOfTeethInputConfig, radialThicknessInputConfig],
   },
   {
     image: "/images/gears/rack.png",
     title: "Rack",
     description: "Transform rotational motion into linear",
+    inputConfigs: [rackModuleInputConfig, rackPressureAngleInputConfig, rackBaseHeightInputConfig, rackLengthInputConfig, rackNumberOfTeethInputConfig],
   },
   {
     image: "/images/gears/spur.png",
     title: "Spur Gear",
     description: "A staple in machinery; simple but noisy.",
+    inputConfigs: [moduleInputConfig, pressureAngleInputConfig, numberOfTeethInputConfig],
   },
 ];
--- src/app/generators/stl/config.ts
+++ src/app/generators/stl/config.ts
@@ -2,59 +2,65 @@ import {
   moduleInputConfig,
   pressureAngleInputConfig,
   numberOfTeethInputConfig,
-  lengthInputConfig
+  lengthInputConfig,
+  doubleHelicalLengthInputConfig,
+  helicalLengthInputConfig,
+  rackModuleInputConfig,
+  rackPressureAngleInputConfig,
+  rackNumberOfTeethInputConfig,
+  rackWidthInputConfig,
+  rackBaseHeightInputConfig,
+  radialThicknessInputConfig,
 } from "@/components/Form/Inputs/configs";
 
-const doubleHelicalLengthInputConfig = {...lengthInputConfig, helpImage: "/images/gears/inputs/double_helical_length.svg"}
-const helicalLengthInputConfig = {...lengthInputConfig, helpImage: "/images/gears/inputs/helical_length.svg"}
 
 export const cardsData = [
   {
     image: "/images/gears/double_helical.png",
     title: "Double Helical Gear",
-    description: "All the benefits, none of the cons (except for price)",
+    description: "High cost and hard to manufacture, but holds none of its helical counterpart's drawbacks",
     inputConfigs: [moduleInputConfig, pressureAngleInputConfig, numberOfTeethInputConfig, doubleHelicalLengthInputConfig],
   },
   {
     image: "/images/gears/spur.png",
     title: "Spur Gear",
-    description: "A staple in machinery; simple but noisy.",
+    description: "A staple in machinery; simple yet elegant",
     inputConfigs: [moduleInputConfig, pressureAngleInputConfig, numberOfTeethInputConfig, lengthInputConfig],
   },
   {
     image: "/images/gears/helical.png",
     title: "Helical Gear",
-    description: "Smooth contact, quiet, but generates axial force.",
+    description: "Rolls with smooth contact, quieter, but generates axial force",
     inputConfigs: [moduleInputConfig, pressureAngleInputConfig, numberOfTeethInputConfig, helicalLengthInputConfig],
   },
   {
     image: "/images/gears/internal_double_helical.png",
     title: "Internal Double Helical Gear",
-    description: "Something else",
-    inputConfigs: [],
+    description: "Ring variant of the double helical, it's more common two see two opposite helical rings",
+    inputConfigs: [moduleInputConfig, pressureAngleInputConfig, numberOfTeethInputConfig, doubleHelicalLengthInputConfig, radialThicknessInputConfig],
   },
   {
     image: "/images/gears/internal_spur.png",
     title: "Internal Spur Gear",
-    description: "Something else",
-    inputConfigs: [],
+    description: "Ring variant of the spur gear, can be used for high torque applications",
+    inputConfigs: [moduleInputConfig, pressureAngleInputConfig, numberOfTeethInputConfig, lengthInputConfig, radialThicknessInputConfig],
   },
   {
     image: "/images/gears/internal_helical.png",
     title: "Internal Helical Gear",
-    description: "Something else",
-    inputConfigs: [],
+    description: "Ring variant of the helical gear, used to be common in automotive transmissions",
+    inputConfigs: [moduleInputConfig, pressureAngleInputConfig, numberOfTeethInputConfig, helicalLengthInputConfig, radialThicknessInputConfig],
   },
   {
     image: "/images/gears/rack.png",
     title: "Rack",
     description: "Transform rotational motion into linear",
-    inputConfigs: [],
+    inputConfigs: [rackModuleInputConfig, rackPressureAngleInputConfig, rackNumberOfTeethInputConfig, rackWidthInputConfig, rackBaseHeightInputConfig],
   },
   {
     image: "/images/gears/bevel.png",
     title: "Straight Bevel Gears",
-    description: "Transfer power between perpendicular axes",
-    inputConfigs: [],
+    description: "Modeled in pairs, they transfer power between perpendicular axes",
+    inputConfigs: [moduleInputConfig, pressureAngleInputConfig],
   },
 ];
