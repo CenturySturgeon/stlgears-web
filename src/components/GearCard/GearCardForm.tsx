938f7bff58bd8c838c72c49c7c225c278792237e
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2026-05-06 21:39:22 -0600

    Forms sectioned in accordions, all have hole params (to be removed)

--- src/components/GearCard/GearCardForm.tsx
+++ src/components/GearCard/GearCardForm.tsx
@@ -1,7 +1,9 @@
 import { useForm } from '@mantine/form';
 import { InputConfig } from '@/types/inputConfigs';
-import { Button, Center, Container, Grid, NumberInput } from '@mantine/core';
+import { Accordion, Button, Center, Container, Grid, NumberInput } from '@mantine/core';
 import HoverCardInput from '@/components/Form/Inputs/HoverCardInput/HoverCardInput';
+import { HoleTypeSelector } from '../Form/Inputs/HoleSelector/HoleSelector';
+import { IconAdjustmentsCog, IconSettings, IconAdjustmentsAlt, IconAdjustmentsPlus } from '@tabler/icons-react';
 
 export default function GearCardForm({
     gear_type,
@@ -13,6 +15,7 @@ export default function GearCardForm({
     const typeValues = { type: gear_type };
     const initialValues = {
         ...typeValues,
+        holeType: 'none', // Add this line
         ...inputConfigs.reduce((acc, config) => {
             const isNumberInput = config.InputComponent === NumberInput;
             acc[config.inputProps.name] = config.inputProps.defaultValue || (isNumberInput ? 0 : '');
@@ -27,30 +30,55 @@ export default function GearCardForm({
     return (
         <Container size="md">
             <form onSubmit={form.onSubmit((values) => console.log(values))}>
-                <Grid gap="md">
-                    {inputConfigs.map((inputConfig, index) => (
-                        <Grid.Col
-                            span={{ base: 12, sm: 6 }}
-                            key={index}
-                            style={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                justifyContent: 'flex-end',
-                            }}
+                <Accordion variant="filled" defaultValue="photos" order={3}>
+                    <Accordion.Item value="photos">
+                        <Accordion.Control
+                            icon={<IconAdjustmentsAlt size={22} color="var(--mantine-color-dimmed)" />}
                         >
-                            <HoverCardInput
-                                {...inputConfig}
-                                inputProps={{
-                                    ...inputConfig.inputProps,
-                                    ...form.getInputProps(inputConfig.inputProps.name),
-                                }}
-                            />
-                        </Grid.Col>
-                    ))}
-                </Grid>
-                <Center mt="lg">
-                    <Button type="submit">Submit</Button>
-                </Center>
+                            Base Params
+                        </Accordion.Control>
+                        <Accordion.Panel>
+                            <Grid gap="md">
+                                {inputConfigs.map((inputConfig, index) => (
+                                    <Grid.Col
+                                        span={{ base: 12, sm: 6 }}
+                                        key={index}
+                                        style={{
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            justifyContent: 'flex-end',
+                                        }}
+                                    >
+                                        <HoverCardInput
+                                            {...inputConfig}
+                                            inputProps={{
+                                                ...inputConfig.inputProps,
+                                                ...form.getInputProps(inputConfig.inputProps.name),
+                                            }}
+                                        />
+                                    </Grid.Col>
+                                ))}
+                            </Grid>
+                        </Accordion.Panel>
+                    </Accordion.Item>
+
+                    <Accordion.Item value="hole-type">
+                        <Accordion.Control
+                            icon={<IconSettings size={22} color="var(--mantine-color-dimmed)" />}
+                        >
+                            Hole Params
+                        </Accordion.Control>
+                        <Accordion.Panel>
+                            <Grid gap="md">
+                                <HoleTypeSelector form={form} />
+                            </Grid>
+                        </Accordion.Panel>
+                    </Accordion.Item>
+
+                    <Center mt="lg">
+                        <Button type="submit">Submit</Button>
+                    </Center>
+                </Accordion>
             </form>
         </Container>
     );
