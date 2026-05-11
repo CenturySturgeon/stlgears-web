80a5ca74563d3def241b88ced49fe88eb5118b29
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2026-05-11 17:42:28 -0600

    Added gearCard types

---
+++ src/types/gearCards.ts
@@ -0,0 +1,19 @@
+import { InputConfig } from "@/types/inputConfigs";
+
+export type GearCardHeader = {
+    title: string,
+    type: string,
+    image: string,
+    description: string,
+}
+
+export type AccordionSection = {
+    label: string;
+    description: string;
+    icon: React.ComponentType<{ size?: number; color?: string }>;
+    inputs: InputConfig[];
+};
+
+export type GearCardWithForm = GearCardHeader & {
+    formSections: AccordionSection[],
+}
