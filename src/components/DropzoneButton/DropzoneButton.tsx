5204528a14b875b72aca685d92d36e8612fe959d
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2026-06-02 18:39:16 -0600

    Added dropzone component for stl gears

---
+++ src/components/DropzoneButton/DropzoneButton.tsx
@@ -0,0 +1,56 @@
+import { useRef } from 'react';
+import { IconCloudUpload, IconDownload, IconX } from '@tabler/icons-react';
+import { Button, Group, Text, useMantineTheme } from '@mantine/core';
+import { Dropzone } from '@mantine/dropzone';
+import classes from './DropzoneButton.module.css';
+
+export function DropzoneButton() {
+  const theme = useMantineTheme();
+  const openRef = useRef<() => void>(null);
+
+  return (
+    <div className={classes.wrapper}>
+      <Dropzone
+        openRef={openRef}
+        onDrop={(files) => {
+          const file = files[0];
+          console.log(file);
+        }}
+        className={classes.dropzone}
+        radius="md"
+        accept={['model/stl', '.stl']}
+        maxFiles={1}
+        maxSize={30 * 1024 ** 2}
+        aria-label="Drop STL file here"
+      >
+        <div style={{ pointerEvents: 'none' }}>
+          <Group justify="center">
+            <Dropzone.Accept>
+              <IconDownload size={50} color={theme.colors.blue[6]} stroke={1.5} />
+            </Dropzone.Accept>
+            <Dropzone.Reject>
+              <IconX size={50} color={theme.colors.red[6]} stroke={1.5} />
+            </Dropzone.Reject>
+            <Dropzone.Idle>
+              <IconCloudUpload size={50} stroke={1.5} className={classes.icon} />
+            </Dropzone.Idle>
+          </Group>
+
+          <Text ta="center" fw={700} fz="lg" mt="xl">
+            <Dropzone.Accept>Drop stl gear here</Dropzone.Accept>
+            <Dropzone.Reject>Only .stl files accepted</Dropzone.Reject>
+            <Dropzone.Idle>Analyze gear</Dropzone.Idle>
+          </Text>
+
+          <Text className={classes.description}>
+            Drag&apos;n&apos;drop you stl gear here to get its params. We can accept only <i>.stl</i> files that where generated with STLGears.com
+          </Text>
+        </div>
+      </Dropzone>
+
+      <Button className={classes.control} size="md" radius="xl" onClick={() => openRef.current?.()}>
+        Select files
+      </Button>
+    </div>
+  );
+}
